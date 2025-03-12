b6fe8fbee9ce734203d52cce153d87a5819c67af
keshavjatt <thecoder780@gmail.com>
2025-03-12 23:57:16 +0530

    worked on signup,login and logout api

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,13 @@
 import express from "express"
+import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-    res.send("signup route")
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post("/login", (req, res) => {
-    res.send("login route")
-});
-
-router.post("/logout", (req, res) => {
-    res.send("logout route")
-});
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
