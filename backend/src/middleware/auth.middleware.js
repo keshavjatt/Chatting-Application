24e25bb2b0326ad831a7d694d77fad107a29f648
keshavjatt <thecoder780@gmail.com>
2025-03-14 15:30:01 +0530

    worked on updateProfile route

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,33 +1,31 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-export const protectRoute = async(req, res, next) => {
+export const protectRoute = async (req, res, next) => {
     try {
-        const token = req.cookie.jwt
+        const token = req.cookies.jwt;  // ✅ FIXED HERE
 
-        if(!token){
+        if (!token) {
             return res.status(401).json({ message: "Unauthorized - No Token Provided" });
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        if(!decoded){
+        if (!decoded) {
             return res.status(401).json({ message: "Unauthorized - Invalid Token" });
         }
 
         const user = await User.findById(decoded.userId).select("-password");
 
-        if(!user) {
+        if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
 
-        req.user = user
-
-        next()
+        req.user = user;
+        next();
 
     } catch (error) {
         console.log("Error in protectRoute middleware: ", error.message);
         res.status(500).json({ message: "Internal server error" });
     }
 };
-
