d190b261a83ab4f8633d2143c297c8ecc338991f
keshavjatt <thecoder780@gmail.com>
2025-03-16 01:20:24 +0530

    worked on Message Controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,18 +4,20 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT:" + PORT);
-    connectDB()
+    connectDB();
 });
